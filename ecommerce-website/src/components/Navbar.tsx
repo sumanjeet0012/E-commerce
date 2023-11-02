decc358e2c82f85b1368b4029d0a3d09c748b191
sumanjeet0012 <sumanjeet0012@gmail.com>
2023-11-02 18:27:49 +0530

    little modifications in navbar style

--- ecommerce-website/src/components/Navbar.tsx
+++ ecommerce-website/src/components/Navbar.tsx
@@ -3,13 +3,14 @@ import { AiOutlineUser,AiOutlineShoppingCart } from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <div className="sticky top-0 z-10 bg-[#00ff00] ">
-        <div className="container hideden lg:block ">
-            <div className="flex justify-between items-center p-8 ">
-                <h1 className="text-4xl font-medium">E-commerce App</h1>
-                <div className="relative w-full max-w-[500px]">
+    <div className="sticky top-0 z-10 bg-green-400 ">
+        <div className="container ">
+            <div className="flex justify-between items-center py-4">
+                <AiOutlineUser className='lg:hidden icon_wrapper mr-4 ' />
+                <h1 className="  hidden lg:block text-4xl font-medium min-w-[300px]">E-commerce App</h1>
+                <div className="relative w-full mr-4">
                     <input className="bg-[#f2f3f5] border-none outline-none px-6 py-3 rounded-[30px] w-full" type="text" placeholder="Search Product ..."/> 
-                    <BsSearch className="absolute top-0 right-0 mt-4 mr-5 text-grey-500" size={20} />
+                    <BsSearch className="absolute top-0 right-0 mt-4 mr-5  text-grey-500" size={20} />
                 </div>
 
                 <div className="flex gap-4 ">
