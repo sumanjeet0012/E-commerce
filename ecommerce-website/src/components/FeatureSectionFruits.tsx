4d35046419629868aa520fc30abfed84e91934f7
Sumanjeet <sumanjeet0012@gmail.com>
2023-11-03 13:04:33 +0000

    product card corrected

--- ecommerce-website/src/components/FeatureSectionFruits.tsx
+++ ecommerce-website/src/components/FeatureSectionFruits.tsx
@@ -1,46 +1,70 @@
 import ProductCard from "./ProductCard";
 
 const data = [
-    { id: 0, img: 'product__1.webp', name: "Dried Mango", price: "$500", },
-    { id: 0, img: 'product__1.webp', name: "Dried Mango", price: "$500", },
-    { id: 0, img: 'product__1.webp', name: "Dried Mango", price: "$500", },
-    { id: 0, img: 'product__1.webp', name: "Dried Mango", price: "$500", },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
 ];
 
 const FeatureSectionFruits = () => {
   return (
-    <div className='container pt-16'>
-        <div className='lg:flex justify-between item-center '>
-            <div>
-                <h3 className='font-medium text-2xl'>Fruits & vegetables</h3>
-                <p className='text-gray-600 mt-2'
-                >Buy farm fresh fruits and vegetables online at the best prices
-                </p>
-            </div>
-
-            <div className='space-x-4 mt-8 lg:mt-0'>
-                <button className='feature_btn'>Fruits</button>
-                <button className="text-gray-600 hover:text-accent">Vegetables</button>
-                <button className="text-gray-600 hover:text-accent">Bread & Bakery</button>
-            </div>
-
+    <div className="container pt-16">
+      <div className="lg:flex justify-between item-center ">
+        <div>
+          <h3 className="font-medium text-2xl">Fruits & vegetables</h3>
+          <p className="text-gray-600 mt-2">
+            Buy farm fresh fruits and vegetables online at the best prices
+          </p>
         </div>
 
-        <div className='grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 pt-8 gap-2'>
-            <img className='w-full h=full object-cover' src="/feature__1.webp" alt="banner" />
+        <div className="space-x-4 mt-8 lg:mt-0">
+          <button className="feature_btn">Fruits</button>
+          <button className="text-gray-600 hover:text-accent">
+            Vegetables
+          </button>
+          <button className="text-gray-600 hover:text-accent">
+            Bread & Bakery
+          </button>
         </div>
+      </div>
 
+      <div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 pt-8 gap-2">
+        {/* <img
+          className="w-full h=full object-cover"
+          src="/feature__1.webp"
+          alt="banner"
+        /> */}
         {data.map((item) => (
-            <ProductCard
+          <ProductCard
             key={item.id}
             img={item.img}
             name={item.name}
             price={item.price}
-            />
-        )) };
-
+          />
+        ))}
+        ;
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FeatureSectionFruits
+export default FeatureSectionFruits;
--- ecommerce-website/src/components/ProductCard.tsx
+++ ecommerce-website/src/components/ProductCard.tsx
@@ -1,36 +1,32 @@
-import { AiFillstar, AiOutlineStar , AiOutlineShopping} from "react-icons/ai";
+import { AiFillStar, AiOutlineStar, AiOutlineShopping } from "react-icons/ai";
 
 interface propsType {
-    img: string
-    name: string
-    price: string
+  img: string;
+  name: string;
+  price: string;
 }
 
 const ProductCard: React.FC<propsType> = ({ img, name, price }) => {
+  return (
+    <div className=" border border-gray-200 hover:border-gray-300 hover:scale-105 transition-transform rounded-lg relative">
+      <img src={img} alt={name} />
+      <div className="space-y-2 relative p-4">
+        <div className="text-yellow-400 flex gap-[2px] text-[20px]">
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiOutlineStar />
+        </div>
+        <h3 className="text-xl font-medium">{name}</h3>
+        <h3 className="text-2xl font-medium text-red-600">{price}</h3>
 
-    return (
-        <div className="border border-gray-200 hover:border-gray-300 hover:scale-105 transition-transform rounded-lg relative">
-            <img src={img} alt={name} />
-            <div className="space-y-2 relative p-4">
-                <div className="text-yellow-400 flex gap-[2px] text-[20px]">
-                    <AiFillstar />
-                    <AiFillstar />
-                    <AiFillstar />
-                    <AiFillstar />
-                    <AiOutlineStar />
+        <button className="absolute -top-4 right-2 bg-accent text-white text-[28px] w-[50px] h-[50px] rounded-full grid place-items-center cursor pointer">
+          <AiOutlineShopping />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-
-                </div>
-                <h3 className="text-xl font-medium">{name}</h3>
-                <h3 className="text-2xl font-medium text-red-600">{price}</h3>
-
-                <button className="absolute -top-4 right-2 bg-accent text-white text-[28px] w-[50px] h-[50px] rounded-full grid place-items-center cursor pointer">
-                    <AiOutlineShopping />
-                </button>
-
-            </div>
-            </div>
-            
-    )
-
-}
+export default ProductCard;
