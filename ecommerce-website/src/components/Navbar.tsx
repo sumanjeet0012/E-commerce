9ded2697983d56b1f9abb1101e5872b1781b238f
sumanjeet0012 <sumanjeet0012@gmail.com>
2023-11-04 11:17:11 +0530

    icon changed

--- ecommerce-website/src/components/Navbar.tsx
+++ ecommerce-website/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { BsSearch } from 'react-icons/bs'
 import { AiOutlineUser,AiOutlineShoppingCart } from 'react-icons/ai'
+import {SiFlipkart} from 'react-icons/si'
 import CartCountBadge from './CartCountBadge'
 
 const Navbar = () => {
@@ -7,7 +8,7 @@ const Navbar = () => {
     <div className="sticky top-0 z-10 bg-green-400 ">
         <div className="container ">
             <div className="flex justify-between items-center py-4">
-                <AiOutlineUser className='lg:hidden icon_wrapper mr-4 ' />
+                <SiFlipkart className='lg:hidden icon_wrapper mr-4 ' />
                 <h1 className="  hidden lg:block text-4xl font-medium min-w-[300px]">E-commerce App</h1>
                 <div className="relative w-full mr-4">
                     <input className="bg-[#f2f3f5] border-none outline-none px-6 py-3 rounded-[30px] w-full" type="text" placeholder="Search Product ..."/> 
